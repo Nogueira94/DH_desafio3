07b2dd93b4688e2526a4cfff77495c01e045a028
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2020-12-01 20:01:54 -0300

    layouts

---
+++ app/src/main/java/com/example/desafiowebservice/ComicDetail.kt
@@ -0,0 +1,11 @@
+package com.example.desafiowebservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComicDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comic_detail)
+    }
+}
--- app/src/main/java/com/example/desafiowebservice/HomeActivity.kt
+++ app/src/main/java/com/example/desafiowebservice/HomeActivity.kt
@@ -2,10 +2,17 @@ package com.example.desafiowebservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvComics.layoutManager = GridLayoutManager(this,3)
+        rvComics.setHasFixedSize(true)
+
     }
 }
